f99fd4e97feb8c58882e799fa9f3b24ceca37376
AliRadmanesh <ali.radmanesh1995@gmail.com>
2023-06-04 13:19:49 +0330

    feat: Create `markdown` library to parse and render markdown content

---
+++ libs/markdown/src/lib/markdown.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+import matter from 'gray-matter';
+import { join } from 'path';
+import { serialize } from 'next-mdx-remote/serialize';
+
+export function getParsedFileContentBySlug(
+  fileName: string,
+  postsPath: string
+) {
+  const postFilePath = join(postsPath, `${fileName}.mdx`);
+  const fileContent = readFileSync(postFilePath);
+
+  const { data, content } = matter(fileContent);
+
+  return {
+    frontMatter: data,
+    content,
+  };
+}
+
+export function renderMarkdown(markdownContent: string) {
+  return serialize(markdownContent || '');
+}
