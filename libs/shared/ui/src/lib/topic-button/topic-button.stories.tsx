e2f937bfcb9af0dde59464cd93acfd5fa0aaf470
AliRadmanesh <ali.radmanesh1995@gmail.com>
2023-06-04 19:01:49 +0330

    feat: config storybook for shared-ui library

---
+++ libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { TopicButton } from './topic-button';
+
+const Story: Meta<typeof TopicButton> = {
+  component: TopicButton,
+  title: 'TopicButton',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
