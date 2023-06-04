28d20de60d5a78585291822f61b407e883727790
AliRadmanesh <ali.radmanesh1995@gmail.com>
2023-06-04 19:46:10 +0330

    Write e2e tests for our site application

--- apps/site-e2e/src/e2e/app.cy.ts
+++ apps/site-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,13 @@
 import { getGreeting } from '../support/app.po';
 
 describe('site', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/blog/dynamic-routing'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should render the title of the post', () => {
+    cy.get('h1').should('contain', 'Dynamic Routing and Static Generation');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome site');
+  it('should properly render the embedded Youtube component', () => {
+    cy.get('iframe').should('be.visible');
   });
 });
